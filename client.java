38f425cd45eedfb37be31112b5ed1b7499e77158
Madura Marathe <marathe.madura@gmail.com>
2018-05-25 00:39:49 -0700

    Adding basic Functionality to server

---
+++ client.java
@@ -0,0 +1,33 @@
+package serverClient;
+
+import java.io.*;
+import java.net.*;
+
+
+public class client{
+	public static void main(String[] args) {
+		clientMethod();
+	}
+	public static void clientMethod(){
+		Socket cSock = null;
+		try{
+			cSock = new Socket("127.0.0.1",3600);// connect to the server
+		}
+		catch (IOException e){
+			System.out.println("unknown host exception");
+		}
+		try{
+			PrintWriter pw = new PrintWriter(cSock.getOutputStream(),true);
+			BufferedReader br = new BufferedReader(new InputStreamReader(cSock.getInputStream()));
+			pw.write("Hello");
+		}
+		catch(IOException e){
+			System.out.println("IO exception");
+		}
+		
+	}
+	
+	
+	
+	
+}
---
+++ server.java
@@ -0,0 +1,49 @@
+package serverClient;
+import java.net.*;
+import java.io.*;
+
+public class server{
+	public static void main(String[] args) {
+		System.out.println("hello world");
+		serverMethod();
+	}
+	public static void serverMethod(){
+		ServerSocket sSocket = null;
+		String line;
+		
+		try{
+			sSocket = new ServerSocket(3600);
+		}
+		catch(Exception e){
+			System.out.println("Caught Exception while creating server socket, could not listen on port 3600  "+e);
+			System.exit(1);
+		}
+		
+		Socket cSocket = null;
+		try{
+			cSocket = sSocket.accept();
+		}
+		catch(Exception e){
+			System.out.println("Accept failed "+e);
+			System.exit(1);
+		}
+		try{
+			PrintWriter out = new PrintWriter(cSocket.getOutputStream(),true);
+			BufferedReader in = new BufferedReader(new InputStreamReader(cSocket.getInputStream()));
+			while(true){
+				try{
+					line = in.readLine();
+					out.print(line);
+				
+				}
+				catch(Exception e){
+					System.out.println("Receiving messages failed");
+				}
+			}	
+		}
+		catch(IOException e){
+			System.out.println("IO Exception occurred");
+		}
+		
+	}
+}
